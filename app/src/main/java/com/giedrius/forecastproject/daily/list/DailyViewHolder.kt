3fd1abc7b303b0ae420f036be1f0b99122faa55d
Giedrius <slikas.giedrius@gmail.com>
2019-12-20 15:56:51 +0200

    Daily screen UI completed

--- app/src/main/java/com/giedrius/forecastproject/daily/list/DailyViewHolder.kt
+++ app/src/main/java/com/giedrius/forecastproject/daily/list/DailyViewHolder.kt
@@ -6,15 +6,21 @@ import com.giedrius.forecastproject.daily.network.DailyForecasts
 import com.giedrius.forecastproject.utils.extensions.fahrenheitToCelciusString
 import com.giedrius.forecastproject.utils.extensions.fahrenheitToCelciusTemp
 import com.giedrius.forecastproject.utils.extensions.formateDateToMonthsAndDays
-import kotlinx.android.synthetic.main.item_daily_forecast.view.label1
-import kotlinx.android.synthetic.main.item_daily_forecast.view.label2
+import com.giedrius.forecastproject.utils.extensions.resIdByName
+import kotlinx.android.synthetic.main.item_daily_forecast.view.iw_conditions
+import kotlinx.android.synthetic.main.item_daily_forecast.view.tv_conditions_description
+import kotlinx.android.synthetic.main.item_daily_forecast.view.tv_temperature
+import kotlinx.android.synthetic.main.item_daily_forecast.view.tv_time
 
 class DailyViewHolder (view: View) : RecyclerView.ViewHolder(view) {
 
     fun bind(forecast: DailyForecasts) {
-        itemView.label1.text = formateDateToMonthsAndDays(forecast.date)
-        itemView.label2.text = "${fahrenheitToCelciusTemp(forecast.temperature.minimum.value)}" +
-                " ${fahrenheitToCelciusString()}" + "/" +
+        val image: Int = itemView.context.resIdByName("ic_${forecast.day.icon}")
+        itemView.iw_conditions.setBackgroundResource(image)
+        itemView.tv_time.text = formateDateToMonthsAndDays(forecast.date)
+        itemView.tv_conditions_description.text = forecast.day.iconPhrase
+        itemView.tv_temperature.text = "${fahrenheitToCelciusTemp(forecast.temperature.minimum.value)}" +
+                " ${fahrenheitToCelciusString()}" + " / " +
                 "${fahrenheitToCelciusTemp(forecast.temperature.maximum.value)}" +
                 " ${fahrenheitToCelciusString()}"
     }
